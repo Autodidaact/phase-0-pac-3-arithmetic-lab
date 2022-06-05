dc23a778a130a13199e6a6724ffa312ce8050b59
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-06-05 22:48:35 +0000

    Modified index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(x, y){
+    return x + y;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(m, n){
+    return m * n;
+}
+function divide(x, y){
+    return x/y;
+}
+function increment(n){
+    return n+1;
+}
+function decrement(n){
+    return n-1;
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
